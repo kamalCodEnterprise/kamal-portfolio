e971a36359d65889bbdaaad2b6169ae06821a53d
kamalCodEnterprise <97885287+kamalCodEnterprise@users.noreply.github.com>
2022-09-20 22:36:43 -0700

    ghfgh

--- src/container/Header/Header-CODENTERPRISE.jsx
+++ src/container/Header/Header-CODENTERPRISE.jsx
@@ -34,7 +34,7 @@ const Header = () => (
 
         <div className="tag-cmp app__flex">
           <p className="p-text">Web Developer</p>
-          <p className="p-text">Freelancer</p>
+          <p className="p-text">Freelancer.</p>
         </div>
       </div>
     </motion.div>
